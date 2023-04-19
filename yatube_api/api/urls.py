85019e2372836cb3639a6301b6311ef67922c0b5
Сергей Яковлев <neznaikaknazvatsy@yandex.ru>
2023-04-19 12:01:53 +0300

    First review

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -17,5 +17,5 @@ router.register(
 
 urlpatterns = [
     path('v1/', include(router.urls)),
-    path('/api/v1/api-token-auth/', views.obtain_auth_token),
+    path('v1/api-token-auth/', views.obtain_auth_token),
 ]
